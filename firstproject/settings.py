13bf63ee52274929585eeadcc74d5979f1936804
summaiya14 <summaiyamahmod@gmail.com>
2021-03-05 14:13:02 +0500

    working with templates and urls

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'firstproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/Dev/home/trydjango/src/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- pages/views.py
+++ pages/views.py
@@ -5,8 +5,10 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
-    return HttpResponse("<h1>Hello Summaiya</h1>")
+    #return HttpResponse("<h1>Hello Summaiya</h1>")
+    return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello Contact</h1>")
+    #return HttpResponse("<h1>Hello Contact</h1>")
+    return render(request, "contact.html", {})
 

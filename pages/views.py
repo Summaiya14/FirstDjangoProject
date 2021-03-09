898bc45bb78f6189dca22b89587264baed7626e7
summaiya14 <summaiyamahmod@gmail.com>
2021-03-09 13:13:45 +0500

    get and post request

--- pages/views.py
+++ pages/views.py
@@ -16,3 +16,10 @@ def about_view(request, *args, **kwargs):
     #return HttpResponse("<h1>Hello Contact</h1>")
     return render(request, "about.html", {})
 
+def expression_view(request):
+    a = int(request.POST['text1'])
+    b = int(request.POST['text2'])
+    c = a + b
+    return render(request, "output.html", {"result": c})
+
+    
